a9fb82bf91808257b9c94c0694cc23a5a28b9d3a
TNCP06 <ctionusa6@gmail.com>
2023-09-06 12:04:32 +0700

    commit kedua

---
+++ TugasDaspro1.java
@@ -0,0 +1,10 @@
+public class TugasDaspro1 {
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama    : Tionusa Catur Pamungkas");
+        System.out.println("Absen   : 27");
+        System.out.println("Kelas   : 1D");
+        System.out.println("Prodi   : Teknik Informatika");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
